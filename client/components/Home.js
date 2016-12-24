7cfa0ec4d41a2de2c1a7e84e78b27ba236c2d5f1
edsant1 <edwins10329@gmail.com>
2016-12-23 19:46:13 -0700

    working on function to iterate through arr of objects for specific provider

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import $ from 'jquery';
+import Twitter from './Twitter';
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
+		// this.getTweets = this.getTweets.bind(this);
+		this.grabAllTweets = this.grabAllTweets.bind(this);
 		this.state = { socialMediaAct: [] };
 	}
 
@@ -17,13 +20,41 @@ class Home extends React.Component {
 		})
 	}
 
+	grabAllTweets() {
+		let socialMediaProvider = this.state.socialMediaAct;
+		const twitterTweet = [];
+		for(let property in socialMediaProvider) {
+			if(socialMediaProvider[property].provider == "twitter") {
+				twitterTweet.push(property);
+			}
+		}
+		return twitterTweet;
+	}
+
+	// getTweets() {
+	// 	const tweets = this.state.socialMediaAct;
+	// 	return tweets.map( (tweet) => {
+	// 		<Twitter username={tweet.actor_username} content={tweet.activity_message}
+	// 		 date={tweet.activity_date} key={tweet.id} />
+	// 	});
+	// }
+
 	render() {
 		return(
 			<div>
 				<h2>Hello</h2>
+				{this.grabAllTweets()}
 			</div>
 		)
 	}
 }
 
 export default Home;
+//{"id":"4958613633","actor_username":"mitchel.hilpert","actor_description":"Cloned multimedia data-warehouse",
+//"actor_name":"Mrs. Domenick Wiza","actor_avator":"https://robohash.org/mitchel.hilpert.png?size=300x300\u0026set=set1"
+//,"actor_url":"https://twitter.com/mitchel.hilpert","provider":"twitter",
+//"activity_url":"https://twitter.com/mitchel.hilpert/4958613633","activity_latitude":"-0.3097010719843496",
+//"activity_longitude":"76.88793327501287","activity_date":"2016-12-22",
+//"activity_message":" https://placehold.it/500x500.jpeg/ffffff/000","activity_likes":7,
+//"activity_shares":12,"activity_comments":4,"activity_attachment":"https://placehold.it/500x500.jpeg/ffffff/000","activity_attachment_type":"image/jpeg","activity_sentiment":0}
+
--- client/components/Twitter.js
+++ client/components/Twitter.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class Twitter extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
 	render() {
 		return(
 			<div>
-				<div className=" col s12 m5">
-					
-				</div>
+				<div className="tweet col s12 m5">
+					<p className="tweet-username">{this.props.actor_username}</p>
+					<p className="tweet-content">{this.props.activity_message}</p>
+					<p className="tweet-date">{this.props.activity_date}</p>
+				</div>	
 			</div>
 		)
 	}
 }
 
 export default Twitter;
+
+//Analysts using our product typically need to know what social media “provider’ it is from
+// (e.g. Twitter, Facebook, Instagram, Reddit), the username on that social media site,
+// the content of the activity, including media, and when it was posted
